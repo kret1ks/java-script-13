f587ebdf9c3817fec963b0e04ca537813b65770e
Nazar Bekerskij <bekerskijnazar9@gmail.com>
2025-10-04 21:00:44 +0300

    123

--- js.js
+++ js.js
@@ -1,3 +1,9 @@
+// додає поле mood зі значенням 'happy'
+// замінює значення hobby на 'skydiving'
+// замінює значення premium на false
+// виводить вміст об'єкта user в форматі ключ:значення використовуючи Object.keys() і for...of
+
+
 const user = {
   name: "Mango",
   age: 20,
@@ -5,8 +11,10 @@ const user = {
   premium: true,
 };
 
-const {name, age, hoby: hobyNew = "skydiving", premium: premiumNew = false, mood = "happy"} = user
+const {name, age, hoby: newHoby = "skydiving", premium: newPremium = false, mood = "happy"} = user
+const newUser = {name, age, hoby: newHoby, premium: newPremium, mood};
 
-
-const newUser = {name, age, hoby: hobyNew, premium: premiumNew, mood};
-console.log(newUser);
+for(const key of Object.keys(newUser)){
+  console.log(`${key}: ${newUser[key]}`);
+  
+}
