aca77241fecb4a62e43ef1144023865e6abb90cf
Nazar Bekerskij <bekerskijnazar9@gmail.com>
2025-10-05 17:05:40 +0300

    123

--- js.js
+++ js.js
@@ -148,3 +148,140 @@ return values
 
 console.log(getAllPropValues(baza, "name")); 
 console.log(getAllPropValues(baza, "age")); 
+
+
+
+
+
+/*
+ * Типів транзацкій всього два.
+ * Можна покласти або зняти гроші з рахунку.
+ */
+const Transaction = {
+  DEPOSIT: 'deposit',
+  WITHDRAW: 'withdraw',
+};
+
+/*
+ * Кожна транзакція - це об'єкт з властивостями: id, type і amount
+ */
+const account = {
+  // Поточний баланс рахунку
+  balance: 0,
+  // лічильник транзакції
+  nextId: 1, 
+  // Історія транзакцій
+
+  transactions: [],
+  /*
+   * Метод створює і повертає об'єкт транзакції.
+   * Приймає суму і тип транзакції.
+   */
+
+  createTransaction(amount, type) {
+    const transaction = {
+      amount: amount,
+      type: type,
+      id: this.nextId
+    }
+    this.nextId += 1;
+    return transaction;
+  },
+  /*
+   * Метод відповідає за додавання суми до балансу.
+   * Приймає суму танзакції.
+   * Викликає createTransaction для створення об'єкта транзакції
+   * після чого додає його в історію транзакцій
+   */
+  deposit(amount) {
+    this.balance += amount
+    const transaction = this.createTransaction(amount, Transaction.DEPOSIT)
+    this.transactions.push(transaction)
+  },
+
+  /*
+   * Метод відповідає за зняття суми з балансу.
+   * Приймає суму танзакції.
+   * Викликає createTransaction для створення об'єкта транзакції
+   * після чого додає його в історію транзакцій.
+   *
+   * Якщо amount більше, ніж поточний баланс, виводь повідомлення
+   * про те, що зняття такої суми не можливо, недостатньо коштів.
+   */
+  withdraw(amount) {
+    if(amount > this.balance){
+      alert("Недостатьно коштів")
+      return 
+    }
+    this.balance -= amount
+    const transaction = this.createTransaction(amount, Transaction.WITHDRAW)
+    this.transactions.push(transaction)
+  },
+
+  /*
+   * Метод повертає поточний баланс
+   */
+  getBalance() {
+    return this.balance
+  },
+  /*
+   * Метод шукає і повертає об'єкт транзакції по id
+   */
+  getTransactionDetails(id) {
+    for(let i = 0; i < this.transactions.length; i += 1){
+      const transaction = this.transactions[i]
+      if(transaction.id === id){
+        return transaction
+      }
+    } 
+  },
+
+  /*
+
+   * Метод повертає кількість коштів
+   * певного типу транзакції з усієї історії транзакцій
+   */
+  getTransactionTotal(type) {
+    let total = 0;
+    for(let i = 0; i < this.transactions.length; i += 1){
+      if(this.transactions[i].type === type){
+         total += this.transactions[i].amount
+      }
+    }
+    return total
+  },
+};
+
+account.deposit(1000)
+console.log("Сума після поповнення", account.getBalance());
+
+account.deposit(500)
+console.log("Сума після поповнення", account.getBalance());
+account.deposit(100)
+console.log("Сума після поповнення", account.getBalance());
+account.deposit(700)
+console.log("Сума після поповнення", account.getBalance());
+
+
+
+
+
+account.withdraw(120)
+console.log("Сума після зняття", account.getBalance());
+account.withdraw(600)
+console.log("Сума після зняття", account.getBalance());
+// console.log(account.getTransactionDetails(3));
+// console.log(account.getTransactionDetails(5));
+
+// console.log(account.transactions);
+// console.log(account.getTransactionTotal("deposit"));
+// console.log(account.getTransactionTotal("withdraw"));
+
+const  totalDeposit = account.getTransactionTotal("deposit")
+console.log(`загальна сума поповнення ${totalDeposit} гривень`);
+
+const  totalWithdraw = account.getTransactionTotal("withdraw")
+console.log(`загальна сума зняття ${totalWithdraw} гривень`);
+
+const TransactionDetails = account.getTransactionDetails(5)
+console.log(`детальна по транзакції`, TransactionDetails);
